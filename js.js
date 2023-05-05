78c29f633949ca4808f3cf90a523c08c72c9a1f6
Anoka Daniel Ugochukwu <106391688+DanielAnoka@users.noreply.github.com>
2023-05-05 09:41:22 +0100

    Add files via upload

---
+++ js.js
@@ -0,0 +1,20 @@
+var password = document.getElementById("password");
+ function genPassword(){
+    let chars =
+    `1234567890qwertyuiopasdfghjklzxcvbnm,.#@$&()!^QWERTYUIOPLKJHGFDSAZXCVBNM`
+
+  let passwordLength = 12;
+
+  let password = "";
+ for (let i = 0; i <= passwordLength; i++) {
+    let randomNumber = Math.floor(Math.random() * chars.length) 
+    password += chars .substring(randomNumber, randomNumber + 1)
+ }
+ document.getElementById('password').value = password
+ }
+ function copyPassword() {
+    let copyText = document.querySelector('#password')
+    copyText.select()
+    copyText.setSelectionRange(0, 999)
+    document.execCommand('copy')
+ }
